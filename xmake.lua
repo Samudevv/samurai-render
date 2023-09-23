008e7c5f436374d348104d94ebdcd1fdf215ef3f
PucklaJ <jonaas.pucher000000@gmail.com>
2023-09-23 22:30:52 +0200

    Add wayland protocols

--- xmake.lua
+++ xmake.lua
@@ -11,8 +11,8 @@ add_rules("mode.debug", "mode.release")
 target("samurai-render")
     set_kind("static")
     add_packages("wayland")
-    add_headerfiles("src/*.h")
-    add_files("src/*.c")
+    add_headerfiles("src/*.h", "src/wayland/*.h")
+    add_files("src/*.c", "src/wayland/*.c")
 
 if get_config("build_examples") then
     target("blank")
